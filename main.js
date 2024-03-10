ef0e61ecb27e388df65dc6d65fbffdd0c4c90cea
obslot777 <52664896+obslot777@users.noreply.github.com>
2024-03-10 09:48:53 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,99 +1,111 @@
 // グローバルに展開
 phina.globalize();
-/*
-const animation_info = {
-  // アニメーション情報
-  "left": { // アニメーション名
-    "frames": [15,16,17], // フレーム番号範囲
-    "next": "left", // 次のアニメーション
-    "frequency": 6, // アニメーション間隔
-  },
-  "right": { // アニメーション名
-    "frames": [12,13,14], // フレーム番号範囲
-    "next": "right", // 次のアニメーション
-    "frequency": 6, // アニメーション間隔
-  },
-  "up": { // アニメーション名
-    "frames": [9,10,11,10], // フレーム番号範囲
-    "next": "up", // 次のアニメーション
-    "frequency": 6, // アニメーション間隔
-  },
-  "down": { // アニメーション名
-    "frames": [6,7,8,7], // フレーム番号範囲
-    "next": "down", // 次のアニメーション
-    "frequency": 6, // アニメーション間隔
-  },
-};
-*/
-
-var FRAME_INFO = {
-  "width": 64, // 1フレームの画像サイズ（横）
-  "height": 64, // 1フレームの画像サイズ（縦）
-  "cols": 6, // フレーム数（横）
-  "rows": 3, // フレーム数（縦）
-}
-
-var ANIM_INFO = {
-  "walk": { // アニメーション名
-    "frames": [6,7,8,7], // フレーム番号範囲
-    "next": "walk", // 次のアニメーション
-    "frequency": 6, // アニメーション間隔
-  },
-}
 
 var ASSETS = {
   image:{
-    'mika': 'Image\\mikapiyo.png',
-    'nasu': 'Image\\nasupiyo.png',
+    'cat': 'Image\\cat.png',
+    'dog': 'Image\\dog.png',
+    'zou': 'Image\\zou.png',
+    'lion': 'Image\\lion.png',
+    'yagi': 'Image\\yagi.png',
+    'hato': 'Image\\hato.png',
+    'niwatori': 'Image\\niwatori.png',
   },
-  spritesheet: {
-    "mika_ss":{
-      frame: FRAME_INFO,
-      animations: ANIM_INFO,
-    },
-    "nasu_ss":{
-      frame: FRAME_INFO,
-      animations: ANIM_INFO,
-    },
-  },
-
+  
   sound: {
     "se_cat": "Sound\\cat.mp3",
+    "se_dog": "Sound\\dog.mp3",
+    "se_zou": "Sound\\zou.mp3",
     "se_lion": "Sound\\lion.mp3",
+    "se_yagi": "Sound\\yagi.mp3",
+    "se_hato": "Sound\\hato.mp3",
+    "se_niwatori": "Sound\\niwatori.mp3",
   },
 };
+
+function ChangeScaleAndPlaySound(sp, sound_name)
+{
+  SoundManager.play(sound_name);
+  sp.tweener.scaleTo(2.0, 500).play();
+  sp.tweener.scaleTo(1.0, 1000).play();
+}
 
 /*
  * メインシーン
  */
 phina.define("MainScene", {
+
   // 継承
   superClass: 'DisplayScene',
   // 初期化
   init: function() {
     // 親クラス初期化
     this.superInit();
-    // 背景色
-    this.backgroundColor = 'skyblue';
-    // スプライト画像作成
-    var mika = Sprite('mika', 64, 64).addChildTo(this);
-    mika.setPosition(200, 480);
-    var anim_mika = FrameAnimation('mika_ss').attachTo(mika);
-    anim_mika.gotoAndPlay('walk');
+    this.backgroundColor = "skyblue";
 
-    var nasu = Sprite('nasu', 64, 64).addChildTo(this);
-    nasu.setPosition(400, 480);
-    var anim_nasu = FrameAnimation('nasu_ss').attachTo(nasu);
-    anim_nasu.gotoAndPlay('walk');
+    // スプライト画像作成
+    var cat = Sprite('cat').addChildTo(this);
+    cat.setPosition(200, 200);
+    cat.width = 100;
+    cat.height = 120;
+
+    var dog = Sprite('dog').addChildTo(this);
+    dog.setPosition(400, 200);
+    dog.width = 100;
+    dog.height = 100;
+
+    var zou = Sprite('zou').addChildTo(this);
+    zou.setPosition(120, 480);
+    zou.width = 100;
+    zou.height = 100;
     
+    var lion = Sprite('lion').addChildTo(this);
+    lion.setPosition(310, 480);
+    lion.width = 100;
+    lion.height = 100;
+    var yagi = Sprite('yagi').addChildTo(this);
+    yagi.setPosition(500, 480);
+    yagi.width = 100;
+    yagi.height = 100;
+
+    var hato = Sprite('hato').addChildTo(this);
+    hato.setPosition(200, 720);
+    hato.width = 100;
+    hato.height = 100;
+
+    var niwatori = Sprite('niwatori').addChildTo(this);
+    niwatori.setPosition(400, 720);
+    niwatori.width = 100;
+    niwatori.height = 100;
+
     // タッチ可能にする
-    mika.setInteractive(true);
-    mika.onpointstart = function() { 
-      SoundManager.play('se_cat');
+    cat.setInteractive(true);
+    cat.onpointstart = function() { 
+      ChangeScaleAndPlaySound(cat, 'se_cat');
     };
-    nasu.setInteractive(true);
-    nasu.onpointstart = function() { 
-      SoundManager.play('se_lion');
+    dog.setInteractive(true);
+    dog.onpointstart = function() { 
+      ChangeScaleAndPlaySound(dog, 'se_dog');
+    };
+    zou.setInteractive(true);
+    zou.onpointstart = function() { 
+      ChangeScaleAndPlaySound(zou, 'se_zou');
+    };
+    lion.setInteractive(true);
+    lion.onpointstart = function() { 
+      ChangeScaleAndPlaySound(lion, 'se_lion');
+    };
+    yagi.setInteractive(true);
+    yagi.onpointstart = function() { 
+      ChangeScaleAndPlaySound(yagi, 'se_yagi');
+    };
+    hato.setInteractive(true);
+    hato.onpointstart = function() { 
+      ChangeScaleAndPlaySound(hato, 'se_hato');
+    };
+    niwatori.setInteractive(true);
+    niwatori.onpointstart = function() { 
+      ChangeScaleAndPlaySound(niwatori, 'se_niwatori');
     };
 
   },
@@ -115,6 +127,3 @@ phina.main(function() {
   // 実行
   app.run();
 });
-
-
-
